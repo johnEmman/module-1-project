57b83e85ed47037a059e17eb4fba3ab427628214
johnEmman <ty.johnemmanuel.com>
2022-12-10 01:47:37 +0800

    commiting all file

--- main.js
+++ main.js
@@ -16,11 +16,22 @@ export var newsList = [
   "Marie Colvin shined a light on war-torn corners of the world",
 ];
 
+const tempo = newsList;
+
 export function search() {
   //inputValue is the variable that contains the search string
   const inputValue = document.getElementById("search-input").value;
 
   //Write your code here for the search function
+  newsList = tempo;
+  let newArray = [];
+  newsList.forEach(lists => {
+    const userInput = lists.toLowerCase().search(inputValue.toLowerCase());
+    if(userInput > -1){
+      newArray.push(lists);
+      newsList = newArray
+    }
+  })
 
   return newsList;
 }
@@ -28,8 +39,28 @@ export function search() {
 export function sort(type) {
   if (type == "ascending") {
     //Write your code here for sorting (ascending)
+    newsList.sort((a, b) => {
+      let aWRegex = a.replace(/[\W_]+/, "");
+      let bWRegex = b.replace(/[\W_]+/, "")
+      if(aWRegex < bWRegex){
+        return -1;
+      }else if(aWRegex > bWRegex){
+        return 1;
+      }
+      return 0;
+    })
   } else {
     //Write your code here for sorting (descending)
+    newsList.sort((a, b) => {
+      let aWRegex = a.replace(/[\W_]+/, "");
+      let bWRegex = b.replace(/[\W_]+/, "")
+      if(aWRegex < bWRegex){
+        return -1;
+      }else if(aWRegex > bWRegex){
+        return 1;
+      }
+      return 0;
+    }).reverse();
   }
 
   return newsList;
